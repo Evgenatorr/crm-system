5eb1022d00072e4a65e149232d559b9612fab4b0
evgenator <zenyavolobuev@mail.ru>
2025-09-08 20:29:41 +0700

    add model for products

--- crm_system/products/models.py
+++ crm_system/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Модель Product представляет товар, который можно продавать.
+    Attributes:
+        price (DecimalField): Стоимость товара.
+        name (CharField): Название товара.
+        description (TextField): Описание товара.
+    """
+
+    class Meta:
+        ordering = ["name", "price"]
+
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+    name = models.CharField(max_length=30, db_index=True)
+    description = models.TextField(
+        max_length=150,
+        null=False,
+        blank=True,
+    )
